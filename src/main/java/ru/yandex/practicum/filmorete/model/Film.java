83a3359acbb230d6c4f8d783958ae00001d127c8
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2023-07-03 21:37:12 +0300

    Внедрение аннотаций для валидации

--- src/main/java/ru/yandex/practicum/filmorete/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorete/model/Film.java
@@ -2,20 +2,35 @@ package ru.yandex.practicum.filmorete.model;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NonNull;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
+
+import static ru.yandex.practicum.filmorete.exeptions.MessageErrorValidFilm.VALID_ERROR_FILM_NOT_DESCRIPTION;
 
 @Data
 @Builder
 public class Film {
 
+    @Positive
     private Integer id;
 
+    @NotBlank
+    @NonNull
     private final String name;
 
+    @NotBlank
+    @NonNull
+    @Size(max = 200)
     private final String description;
 
+    @NonNull
     private final LocalDate releaseDate;
 
+    @Positive
     private final Integer duration;
 }
--- src/main/java/ru/yandex/practicum/filmorete/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorete/model/User.java
@@ -2,20 +2,33 @@ package ru.yandex.practicum.filmorete.model;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NonNull;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Positive;
 import java.time.LocalDate;
 
 @Data
 @Builder
 public class User {
 
+    @Positive
     private Integer id;
 
     private String name;
 
+    @NonNull
+    @PastOrPresent
     private LocalDate birthday;
 
+    @NotBlank
+    @NonNull
     private String login;
 
+    @Email
+    @NotBlank
+    @NonNull
     private String email;
 }
