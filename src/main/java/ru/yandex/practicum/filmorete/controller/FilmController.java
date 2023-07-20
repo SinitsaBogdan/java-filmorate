724dfe9a9415b4b70fdc33797f8527a8e2878273
SinitsaBogdan <sinitsa.bogdan.1997@yandex.ru>
2023-07-20 20:17:38 +0300

    Обновление контроллеров

--- src/main/java/ru/yandex/practicum/filmorete/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorete/controller/FilmController.java
@@ -3,38 +3,56 @@ package ru.yandex.practicum.filmorete.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorete.exeptions.ValidationFilmException;
+import ru.yandex.practicum.filmorete.exeptions.ValidationUserException;
 import ru.yandex.practicum.filmorete.model.Film;
+import ru.yandex.practicum.filmorete.service.FilmService;
 
 import javax.validation.Valid;
 import java.time.LocalDate;
 import java.util.*;
 
 import static ru.yandex.practicum.filmorete.exeptions.MessageErrorValidFilm.*;
+import static ru.yandex.practicum.filmorete.exeptions.MessageErrorValidUser.VALID_ERROR_USER_ID_NOT_IN_COLLECTIONS;
 
 @Slf4j
 @RequestMapping("/films")
 @RestController
 public class FilmController {
 
-    private final Map<Integer, Film> films = new HashMap<>();
-    private final Set<String> names = new HashSet<>();
-    private Integer lastIdentification = 1;
+    final FilmService filmService;
+
+    public FilmController(FilmService filmService) {
+        this.filmService = filmService;
+    }
 
     @GetMapping()
     public Collection<Film> findAll() {
-        log.info("Запрос всех фильмов: {}", films.size());
-        return films.values();
+        log.info("Запрос всех фильмов: {}", filmService.filmStorage.getFilm().size());
+        return filmService.filmStorage.getFilm();
+    }
+
+    @GetMapping("/{id}")
+    public Film findToId(@PathVariable Long id) {
+
+        if (filmService.filmStorage.getCollectionsIdFilms().contains(id)) {
+            return filmService.filmStorage.getFilm(id);
+        } else {
+            throw new ValidationFilmException(VALID_ERROR_FILM_ID_NOT_IN_COLLECTIONS);
+        }
+    }
+
+    @GetMapping("/popular")
+    public Collection<Film> getPopularFilms(@RequestParam(defaultValue = "10") Integer count) {
+        return filmService.getPopularFilms(count);
     }
 
     @PostMapping()
     public Film create(@Valid @RequestBody Film film) throws ValidationFilmException {
-        if (names.contains(film.getName())) {
-            throw new ValidationFilmException(VALID_ERROR_FILM_DOUBLE_IN_COLLECTIONS, 400);
+        if (filmService.filmStorage.getNames().contains(film.getName())) {
+            throw new ValidationFilmException(VALID_ERROR_FILM_DOUBLE_IN_COLLECTIONS);
         } else {
             validatorFilms(film);
-            film.setId(getLastIdentification());
-            films.put(film.getId(), film);
-            names.add(film.getName());
+            filmService.filmStorage.addFilm(film);
             log.info(String.format("Добавление нового фильма: %s", film.getName()));
             return film;
         }
@@ -43,40 +61,69 @@ public class FilmController {
     @PutMapping()
     public Film update(@Valid @RequestBody Film film) throws ValidationFilmException {
         if (film.getId() == null) {
-            throw new ValidationFilmException(VALID_ERROR_FILM_NOT_ID, 400);
+            throw new ValidationFilmException(VALID_ERROR_FILM_NOT_ID);
         }
-        if (!films.containsKey(film.getId())) {
-            throw new ValidationFilmException(VALID_ERROR_FILM_ID_NOT_IN_COLLECTIONS, 500);
+        if (!filmService.filmStorage.getCollectionsIdFilms().contains(film.getId())) {
+            throw new ValidationFilmException(VALID_ERROR_FILM_ID_NOT_IN_COLLECTIONS);
         }
 
         validatorFilms(film);
-        Film oldFilm = films.get(film.getId());
-        names.remove(oldFilm.getName());
-        names.add(film.getName());
-
-        films.put(film.getId(), film);
+        filmService.filmStorage.updateFilm(film);
         log.info("Обновление фильма: {}", film.getName());
         return film;
     }
 
+    @PutMapping("/{id}/like/{userId}")
+    public void addLikeFilm(@PathVariable Long id, @PathVariable Long userId) {
+
+        if (
+                !filmService.filmStorage.getCollectionsIdFilms().contains(id)
+        ) {
+            throw new ValidationFilmException(VALID_ERROR_FILM_ID_NOT_IN_COLLECTIONS);
+        }
+
+        if (
+                !filmService.userStorage.getCollectionsIdUsers().contains(userId)
+        ) {
+            throw new ValidationUserException(VALID_ERROR_USER_ID_NOT_IN_COLLECTIONS);
+        }
+
+        filmService.addLike(filmService.filmStorage.getFilm(id), filmService.userStorage.getUser(userId));
+    }
+
+    @DeleteMapping("/{id}/like/{userId}")
+    public void removeLikeFilm(@PathVariable Long id, @PathVariable Long userId) {
+
+        if (
+                !filmService.filmStorage.getCollectionsIdFilms().contains(id)
+        ) {
+            throw new ValidationFilmException(VALID_ERROR_FILM_ID_NOT_IN_COLLECTIONS);
+        }
+
+        if (
+                !filmService.userStorage.getCollectionsIdUsers().contains(userId)
+        ) {
+            throw new ValidationUserException(VALID_ERROR_USER_ID_NOT_IN_COLLECTIONS);
+        }
+
+        filmService.removeLike(filmService.filmStorage.getFilm(id), filmService.userStorage.getUser(userId));
+    }
+
     @DeleteMapping()
     public void clear() {
-        films.clear();
-        names.clear();
-        lastIdentification = 1;
+        filmService.filmStorage.clear();
         log.info("Очистка хранилища Фильмов!");
     }
 
     private void validatorFilms(Film film) throws ValidationFilmException {
         if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
-            throw new ValidationFilmException(VALID_ERROR_FILM_RELEASED_MIN, 400);
+            throw new ValidationFilmException(VALID_ERROR_FILM_RELEASED_MIN);
         }
         if (film.getDuration() == null) {
-            throw new ValidationFilmException(VALID_ERROR_FILM_NOT_DURATION, 400);
+            throw new ValidationFilmException(VALID_ERROR_FILM_NOT_DURATION);
         }
-    }
-
-    private Integer getLastIdentification() {
-        return lastIdentification++;
+        if (film.getLikeUsers() == null) {
+            film.setLikeUsers(new HashSet<>());
+        }
     }
 }
--- src/main/java/ru/yandex/practicum/filmorete/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorete/controller/UserController.java
@@ -4,9 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorete.exeptions.ValidationUserException;
 import ru.yandex.practicum.filmorete.model.User;
+import ru.yandex.practicum.filmorete.service.UserService;
 
 import javax.validation.Valid;
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static ru.yandex.practicum.filmorete.exeptions.MessageErrorValidUser.*;
 
@@ -15,26 +17,52 @@ import static ru.yandex.practicum.filmorete.exeptions.MessageErrorValidUser.*;
 @RestController
 public class UserController {
 
-    private final Map<Integer, User> users = new HashMap<>();
-    private final Set<String> emails = new HashSet<>();
-    private Integer lastIdentification = 1;
+    final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping()
     public Collection<User> findAll() {
-        return users.values();
+        return userService.storage.getUser();
+    }
+
+    @GetMapping("/{id}")
+    public User findToId(@PathVariable Long id) {
+
+        if (userService.storage.getCollectionsIdUsers().contains(id)) {
+            return userService.storage.getUser(id);
+        } else {
+            throw new ValidationUserException(VALID_ERROR_USER_ID_NOT_IN_COLLECTIONS);
+        }
+    }
+
+    @GetMapping("/{id}/friends")
+    public List<User> getFriends(@PathVariable Long id) {
+        return userService.storage.getUser(id).getFriends().stream()
+                .map(userService.storage::getUser)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{id}/friends/common/{otherId}")
+    public List<User> getFriendsCommon(@PathVariable Long id, @PathVariable Long otherId) {
+        if (userService.storage.getCollectionsIdUsers().contains(id) && userService.storage.getCollectionsIdUsers().contains(otherId)) {
+            return userService.getSharedFriends(userService.storage.getUser(id), userService.storage.getUser(otherId));
+        } else {
+            throw new ValidationUserException(VALID_ERROR_USER_ID_NOT_IN_COLLECTIONS);
+        }
     }
 
     @PostMapping()
     public User create(@Valid @RequestBody User user) throws ValidationUserException {
-        if (!emails.contains(user.getEmail())) {
+        if (!userService.storage.getEmails().contains(user.getEmail())) {
             validatorUser(user);
-            user.setId(getLastIdentification());
-            users.put(user.getId(), user);
-            emails.add(user.getEmail());
+            userService.storage.addUser(user);
             log.info("Добавлен новый пользователь!");
             return user;
         } else {
-            throw new ValidationUserException(VALID_ERROR_USER_DOUBLE_IN_COLLECTIONS, 400);
+            throw new ValidationUserException(VALID_ERROR_USER_DOUBLE_IN_COLLECTIONS);
         }
     }
 
@@ -42,35 +70,61 @@ public class UserController {
     public User update(@Valid @RequestBody User user) throws ValidationUserException {
 
         if (user.getId() == null) {
-            throw new ValidationUserException(VALID_ERROR_USER_NOT_ID, 400);
+            throw new ValidationUserException(VALID_ERROR_USER_NOT_ID);
         }
-        if (!users.containsKey(user.getId())) {
-            throw new ValidationUserException(VALID_ERROR_USER_ID_NOT_IN_COLLECTIONS, 500);
+        if (!userService.storage.getCollectionsIdUsers().contains(user.getId())) {
+            throw new ValidationUserException(VALID_ERROR_USER_ID_NOT_IN_COLLECTIONS);
         }
         validatorUser(user);
-        users.put(user.getId(), user);
+        userService.storage.updateUser(user);
         log.info("Пользователь {} успешно обновлен!", user.getName());
         return user;
     }
 
+    @PutMapping("/{id}/friends/{friendId}")
+    public void addFriends(@PathVariable Long friendId, @PathVariable Long id) {
+
+        if (
+                !userService.storage.getCollectionsIdUsers().contains(id) ||
+                !userService.storage.getCollectionsIdUsers().contains(friendId)
+        ) {
+            throw new ValidationUserException(VALID_ERROR_USER_ID_NOT_IN_COLLECTIONS);
+        }
+
+        userService.addFriend(userService.storage.getUser(id), userService.storage.getUser(friendId));
+    }
+
+    @DeleteMapping("/{id}/friends/{friendId}")
+    public void deleteFriends(@PathVariable Long friendId, @PathVariable Long id) {
+
+        if (
+                !userService.storage.getCollectionsIdUsers().contains(id) ||
+                !userService.storage.getCollectionsIdUsers().contains(friendId)
+        ) {
+            throw new ValidationUserException(VALID_ERROR_USER_ID_NOT_IN_COLLECTIONS);
+        }
+
+        userService.removeFriend(userService.storage.getUser(id), userService.storage.getUser(friendId));
+    }
+
     @DeleteMapping()
     public void clear() {
-        users.clear();
-        emails.clear();
-        lastIdentification = 1;
+        userService.storage.clear();
         log.info("Очистка хранилища Пользователей!");
     }
 
     private void validatorUser(User user) throws ValidationUserException {
         if (user.getLogin().contains(" ")) {
-            throw new ValidationUserException(VALID_ERROR_USER_LOGIN_IS_WHITESPACE, 400);
+            throw new ValidationUserException(VALID_ERROR_USER_LOGIN_IS_WHITESPACE);
         }
         if (user.getName() == null || user.getName().isEmpty()) {
             user.setName(user.getLogin());
         }
-    }
-
-    private Integer getLastIdentification() {
-        return lastIdentification++;
+        if (user.getFriends() == null) {
+            user.setFriends(new HashSet<>());
+        }
+        if (user.getLikesFilms() == null) {
+            user.setLikesFilms(new HashSet<>());
+        }
     }
 }
