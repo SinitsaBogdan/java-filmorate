f9dfd862e6eecdca02a2c417c3f921cc7b54114c
SinitsaBogdan <sinitsa.bogdan.1997@yandex.ru>
2023-08-12 01:58:45 +0300

    Подготовка проекта

--- src/main/java/ru/yandex/practicum/filmorete/controller/HandlerController.java
+++ src/main/java/ru/yandex/practicum/filmorete/controller/HandlerController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorete.controller;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,34 +8,40 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import ru.yandex.practicum.filmorete.exeptions.*;
 
 @ControllerAdvice
-public class ControllerHandler {
+public class HandlerController {
 
     @ExceptionHandler(ExceptionValidationFilm.class)
-    public ResponseEntity<ErrorResponse> handleException(ExceptionValidationFilm exception) {
+    public ResponseEntity<ErrorResponse> handleException(@NotNull ExceptionValidationFilm exception) {
         ErrorResponse response = new ErrorResponse(exception.getName(), exception.getDescription());
         return new ResponseEntity<>(response, HttpStatus.valueOf(exception.getHttpStatusCode()));
     }
 
     @ExceptionHandler(ExceptionValidationUser.class)
-    public ResponseEntity<ErrorResponse> handleException(ExceptionValidationUser exception) {
+    public ResponseEntity<ErrorResponse> handleException(@NotNull ExceptionValidationUser exception) {
         ErrorResponse response = new ErrorResponse(exception.getName(), exception.getDescription());
         return new ResponseEntity<>(response, HttpStatus.valueOf(exception.getHttpStatusCode()));
     }
 
     @ExceptionHandler(ExceptionNotFoundUserStorage.class)
-    public ResponseEntity<ErrorResponse> handleException(ExceptionNotFoundUserStorage exception) {
+    public ResponseEntity<ErrorResponse> handleException(@NotNull ExceptionNotFoundUserStorage exception) {
         ErrorResponse response = new ErrorResponse(exception.getName(), exception.getDescription());
         return new ResponseEntity<>(response, HttpStatus.valueOf(exception.getHttpStatusCode()));
     }
 
     @ExceptionHandler(ExceptionNotFoundFilmStorage.class)
-    public ResponseEntity<ErrorResponse> handleException(ExceptionNotFoundFilmStorage exception) {
+    public ResponseEntity<ErrorResponse> handleException(@NotNull ExceptionNotFoundFilmStorage exception) {
         ErrorResponse response = new ErrorResponse(exception.getName(), exception.getDescription());
         return new ResponseEntity<>(response, HttpStatus.valueOf(exception.getHttpStatusCode()));
     }
 
-    @ExceptionHandler(ExceptionServiceFilmorate.class)
-    public ResponseEntity<ErrorResponse> handleException(ExceptionServiceFilmorate exception) {
+    @ExceptionHandler(ExceptionNotFoundGenreStorage.class)
+    public ResponseEntity<ErrorResponse> handleException(@NotNull ExceptionNotFoundGenreStorage exception) {
+        ErrorResponse response = new ErrorResponse(exception.getName(), exception.getDescription());
+        return new ResponseEntity<>(response, HttpStatus.valueOf(exception.getHttpStatusCode()));
+    }
+
+    @ExceptionHandler(ExceptionNotFoundMpaStorage.class)
+    public ResponseEntity<ErrorResponse> handleException(@NotNull ExceptionNotFoundMpaStorage exception) {
         ErrorResponse response = new ErrorResponse(exception.getName(), exception.getDescription());
         return new ResponseEntity<>(response, HttpStatus.valueOf(exception.getHttpStatusCode()));
     }
---
+++ src/main/java/ru/yandex/practicum/filmorete/sql/dao/FilmDao.java
@@ -0,0 +1,28 @@
+package ru.yandex.practicum.filmorete.sql.dao;
+
+import ru.yandex.practicum.filmorete.model.Film;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public interface FilmDao {
+
+    Optional<Long> findLastId();
+
+    Optional<List<Film>> findRows();
+    Optional<Film> findRow(Long rowId);
+    Optional<Film> findRow(String filmName);
+
+    void insert(Integer mpaId, String name, String descriptions, LocalDate releaseDate, Integer durationMinute);
+    void insert(Long rowId, Integer mpaId, String name, String descriptions, LocalDate releaseDate, Integer durationMinute);
+
+    void update(Long searchRowId, Integer mpaId, String name, String descriptions, LocalDate releaseDate, Integer duration);
+    void update(String searchName, Integer mpaId, String name, String descriptions, LocalDate releaseDate, Integer duration);
+
+    void delete();
+    void delete(Long rowId);
+    void delete(String name);
+    void delete(LocalDate releaseDate);
+    void delete(Integer duration);
+    void deleteByRating(Integer mpaId);
+}
