1309f275883801d9ae595a3a482d9bc4624f5791
KonstantinTseplyaev <126201513+KonstantinTseplyaev@users.noreply.github.com>
2023-09-01 15:42:44 +0300

    style: правки

--- src/main/java/ru/yandex/practicum/filmorete/service/ServiceDirector.java
+++ src/main/java/ru/yandex/practicum/filmorete/service/ServiceDirector.java
@@ -2,7 +2,7 @@ package ru.yandex.practicum.filmorete.service;
 
 import java.util.List;
 import java.util.Optional;
-import javax.validation.constraints.NotBlank;
+
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorete.exeptions.ExceptionNotFoundDirectorStorage;
@@ -14,11 +14,11 @@ import static ru.yandex.practicum.filmorete.exeptions.MessageErrorServiceDirecto
 @Service
 public class ServiceDirector {
 
-     private final DirectorDao directorDao;
+    private final DirectorDao directorDao;
 
-     private ServiceDirector(DirectorDao directorsDao) {
-         this.directorDao = directorsDao;
-     }
+    private ServiceDirector(DirectorDao directorsDao) {
+        this.directorDao = directorsDao;
+    }
 
     /**
      * Запрос режиссёра из таблицы DIRECTORS по ID [ DIRECTORS ].
@@ -76,55 +76,5 @@ public class ServiceDirector {
      */
     public void deleteSearchName(String directorName) {
         directorDao.delete(directorName);
-    }
-
-    /**
-     * NEW!!!
-     * Запрос всех режиссёров по ID фильма [ TOTAL_FILM_DIRECTOR ].
-     */
-    public void getAllDirectorToFilm(@NotNull Long filmId) {
-    }
-
-    /**
-     * NEW!!!
-     * Запрос всех фильмов по ID режиссёра [ TOTAL_FILM_DIRECTOR ].
-     */
-    public void getAllFilmIsDirector(@NotNull Long directorId) {
-    }
-
-    /**
-     * NEW!!!
-     * Запрос всех фильмов по имени режиссёра [ TOTAL_FILM_DIRECTOR ].
-     */
-    public void getAllFilmIsDirector(@NotNull String directorName) {
-    }
-
-    /**
-     * NEW!!!
-     * Добавление нового режиссёра к сществующему фильму [ TOTAL_FILM_DIRECTOR ].
-     */
-    public void addDirectorToFilm(@NotNull Director director, @NotNull Long filmId) {
-    }
-
-    /**
-     * NEW!!!
-     * Удаление всех режиссёров из сществующего фильма по ID фильма [
-     * TOTAL_FILM_DIRECTOR ].
-     */
-    public void deleteAllDirectorToFilm(@NotNull Long filmId) {
-    }
-
-    /**
-     * NEW!!!
-     * Удаление режиссёра из сществующего фильма по Имени [ TOTAL_FILM_DIRECTOR ].
-     */
-    public void deleteDirectorToFilm(@NotBlank String directorName, @NotNull Long filmId) {
-    }
-
-    /**
-     * NEW!!!
-     * Удаление режиссёра из сществующего фильма по Id [ TOTAL_FILM_DIRECTOR ].
-     */
-    public void deleteDirectorToFilm(@NotBlank Integer directorId, @NotNull Long filmId) {
     }
 }
--- src/main/java/ru/yandex/practicum/filmorete/sql/impl/TotalDirectorFilmDaoImpl.java
+++ src/main/java/ru/yandex/practicum/filmorete/sql/impl/TotalDirectorFilmDaoImpl.java
@@ -30,7 +30,7 @@ public class TotalDirectorFilmDaoImpl implements TotalDirectorFilmDao {
                 "SELECT " +
                         "t.film_id AS filmId, " +
                         "t.director_id AS directorId " +
-                    "FROM TOTAL_FILM_DIRECTOR AS t;"
+                        "FROM TOTAL_FILM_DIRECTOR AS t;"
         );
         while (rows.next()) result.add(buildModel(rows));
         return result;
@@ -52,7 +52,7 @@ public class TotalDirectorFilmDaoImpl implements TotalDirectorFilmDao {
         List<TotalDirectorFilm> result = new ArrayList<>();
         SqlRowSet rows = jdbcTemplate.queryForRowSet(
                 "SELECT * FROM TOTAL_FILM_DIRECTOR " +
-                "WHERE director_id = ?;",
+                        "WHERE director_id = ?;",
                 directorId
         );
         while (rows.next()) result.add(buildModel(rows));
@@ -63,7 +63,7 @@ public class TotalDirectorFilmDaoImpl implements TotalDirectorFilmDao {
     public void insert(Long filmId, Long directorId) {
         jdbcTemplate.update(
                 "INSERT INTO TOTAL_FILM_DIRECTOR (film_id, director_id) " +
-                    "VALUES (?, ?);",
+                        "VALUES (?, ?);",
                 filmId, directorId
         );
     }
@@ -90,9 +90,32 @@ public class TotalDirectorFilmDaoImpl implements TotalDirectorFilmDao {
     public List<Film> findPopularFilmsByDirector(Long directorId) {
         Set<Long> filmsId = new HashSet<>();
         List<Film> result = new ArrayList<>();
-        SqlRowSet rows = jdbcTemplate.queryForRowSet(generateSqlRequest() + "WHERE f.id IN ( SELECT film_id FROM " +
-                "TOTAL_FILM_DIRECTOR WHERE director_id = ?) " +
-                "ORDER BY ( SELECT COUNT(*) FROM TOTAL_FILM_LIKE AS l WHERE l.film_id = f.id ) DESC;", directorId);
+        SqlRowSet rows = jdbcTemplate.queryForRowSet(
+                "SELECT " +
+                        "f.id AS film_id, " +
+                        "f.name AS film_name, " +
+                        "f.description AS film_description, " +
+                        "f.release_date AS film_release_date, " +
+                        "f.duration AS film_duration, " +
+                        "r.id AS mpa_id, " +
+                        "r.name AS mpa_name, " +
+                        "g.id AS genre_id, " +
+                        "g.name AS genre_name, " +
+                        "d.id AS director_id, " +
+                        "d.name AS director_name, " +
+                        "EXTRACT(YEAR FROM f.release_date) AS release_year, " +
+                        "( SELECT COUNT(*) FROM TOTAL_FILM_LIKE AS l WHERE l.film_id = f.id ) AS size_like " +
+                        "FROM FILMS AS f " +
+                        "INNER JOIN ROSTER_MPA AS r ON f.mpa_id = r.id " +
+                        "LEFT JOIN TOTAL_GENRE_FILM AS t ON f.id = t.film_id " +
+                        "LEFT JOIN ROSTER_GENRE AS g ON t.genre_id = g.id " +
+                        "LEFT JOIN TOTAL_FILM_DIRECTOR AS td ON f.id = td.film_id " +
+                        "LEFT JOIN DIRECTORS AS d ON td.director_id = d.id " +
+                        "WHERE f.id IN ( SELECT film_id FROM " +
+                        "TOTAL_FILM_DIRECTOR WHERE director_id = ?) " +
+                        "ORDER BY ( SELECT COUNT(*) FROM TOTAL_FILM_LIKE AS l WHERE l.film_id = f.id ) DESC;",
+                directorId
+        );
         while (rows.next()) {
             Integer genreId = rows.getInt("GENRE_ID");
             String genreName = rows.getString("GENRE_NAME");
@@ -165,29 +188,6 @@ public class TotalDirectorFilmDaoImpl implements TotalDirectorFilmDao {
             filmsId.add(film.getId());
         }
         return result;
-    }
-
-    private String generateSqlRequest() {
-        return "SELECT " +
-                "f.id AS film_id, " +
-                "f.name AS film_name, " +
-                "f.description AS film_description, " +
-                "f.release_date AS film_release_date, " +
-                "f.duration AS film_duration, " +
-                "r.id AS mpa_id, " +
-                "r.name AS mpa_name, " +
-                "g.id AS genre_id, " +
-                "g.name AS genre_name, " +
-                "d.id AS director_id, " +
-                "d.name AS director_name, " +
-                "EXTRACT(YEAR FROM f.release_date) AS release_year, " +
-                "( SELECT COUNT(*) FROM TOTAL_FILM_LIKE AS l WHERE l.film_id = f.id ) AS size_like " +
-                "FROM FILMS AS f " +
-                "INNER JOIN ROSTER_MPA AS r ON f.mpa_id = r.id " +
-                "LEFT JOIN TOTAL_GENRE_FILM AS t ON f.id = t.film_id " +
-                "LEFT JOIN ROSTER_GENRE AS g ON t.genre_id = g.id " +
-                "LEFT JOIN TOTAL_FILM_DIRECTOR AS td ON f.id = td.film_id " +
-                "LEFT JOIN DIRECTORS AS d ON td.director_id = d.id ";
     }
 
     @Override
