084c8e94104a650085ba52b09a7e7a49a445e59a
SinitsaBogdan <sinitsa.bogdan.1997@yandex.ru>
2023-09-03 02:12:32 +0300

    Update Director.java

--- src/main/java/ru/yandex/practicum/filmorete/model/Director.java
+++ src/main/java/ru/yandex/practicum/filmorete/model/Director.java
@@ -4,7 +4,6 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
