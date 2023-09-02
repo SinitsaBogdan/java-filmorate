74fd445fa70b975e37b7b282e7c81950726a474f
allen11137 <122568848+allen11137@users.noreply.github.com>
2023-09-02 20:00:43 +0300

    Рефакторинг классов-исключений
    -Изменение параметров-конструктора (названий)

--- src/main/java/ru/yandex/practicum/filmorete/exeptions/ExceptionNotFoundFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorete/exeptions/ExceptionNotFoundFilmStorage.java
@@ -2,16 +2,18 @@ package ru.yandex.practicum.filmorete.exeptions;
 
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
+import ru.yandex.practicum.filmorete.exeptions.message.FilmErrorMessage;
+import ru.yandex.practicum.filmorete.exeptions.message.ValidFilmErrorMessage;
 
 @Slf4j
 public class ExceptionNotFoundFilmStorage extends AbstractCustomException {
 
-    public ExceptionNotFoundFilmStorage(@NotNull MessageErrorValidFilm error) {
+    public ExceptionNotFoundFilmStorage(@NotNull ValidFilmErrorMessage error) {
         super(error.name, error.description, error.httpStatusCode);
         log.debug(this.getMessage());
     }
 
-    public ExceptionNotFoundFilmStorage(@NotNull MessageErrorServiceFilm error) {
+    public ExceptionNotFoundFilmStorage(@NotNull FilmErrorMessage error) {
         super(error.name, error.description, error.httpStatusCode);
         log.debug(this.getMessage());
     }
--- src/main/java/ru/yandex/practicum/filmorete/exeptions/ExceptionNotFoundMpaStorage.java
+++ src/main/java/ru/yandex/practicum/filmorete/exeptions/ExceptionNotFoundMpaStorage.java
@@ -2,11 +2,12 @@ package ru.yandex.practicum.filmorete.exeptions;
 
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
+import ru.yandex.practicum.filmorete.exeptions.message.MpaErrorMessage;
 
 @Slf4j
 public class ExceptionNotFoundMpaStorage extends AbstractCustomException {
 
-    public ExceptionNotFoundMpaStorage(@NotNull MessageErrorServiceMpa error) {
+    public ExceptionNotFoundMpaStorage(@NotNull MpaErrorMessage error) {
         super(error.name, error.description, error.httpStatusCode);
         log.debug(this.getMessage());
     }
