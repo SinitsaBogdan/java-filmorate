c3d8f04494788f5f0b1b1550bca2a13566669650
Михаил <djmx97@yandex.ru>
2023-09-03 01:08:48 +0300

    fix-project

--- src/main/java/ru/yandex/practicum/filmorete/factory/FactoryModel.java
+++ src/main/java/ru/yandex/practicum/filmorete/factory/FactoryModel.java
@@ -26,7 +26,7 @@ public class FactoryModel {
     public static @NotNull Event buildEvent(@NotNull SqlRowSet row) {
         Long id = row.getLong("ID");
         System.out.println(row.getString("TIMESTAMP"));
-        Long timestamp = row.getLong("TIMESTAMP");
+        Long timestamp = Objects.requireNonNull(row.getTimestamp("TIMESTAMP")).getTime();
         Long userId = row.getLong("USER_ID");
         EventType type = EventType.valueOf(row.getString("TYPE"));
         EventOperation operation = EventOperation.valueOf(row.getString("OPERATION"));
