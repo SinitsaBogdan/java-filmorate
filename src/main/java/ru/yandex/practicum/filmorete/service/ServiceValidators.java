d9d56c4265d7a6e42533ac775b6f9390756f021e
allen11137 <122568848+allen11137@users.noreply.github.com>
2023-09-02 20:08:04 +0300

    Рефакторинг исключений
    - Изменение импортов
    - Изменение названия выбрасываемых исключений:  ExceptionValidationUser на ExceptionValidation, ExceptionValidationFilm на ExceptionValidation

--- src/main/java/ru/yandex/practicum/filmorete/service/ServiceValidators.java
+++ src/main/java/ru/yandex/practicum/filmorete/service/ServiceValidators.java
@@ -2,16 +2,15 @@ package ru.yandex.practicum.filmorete.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
-import ru.yandex.practicum.filmorete.exeptions.ExceptionValidationFilm;
-import ru.yandex.practicum.filmorete.exeptions.ExceptionValidationUser;
+import ru.yandex.practicum.filmorete.exeptions.ExceptionValidation;
 import ru.yandex.practicum.filmorete.model.Film;
 import ru.yandex.practicum.filmorete.model.User;
 
 import java.time.LocalDate;
 
-import static ru.yandex.practicum.filmorete.exeptions.MessageErrorValidFilm.*;
-import static ru.yandex.practicum.filmorete.exeptions.MessageErrorValidUser.VALID_ERROR_USER_LOGIN_IS_WHITESPACE;
-import static ru.yandex.practicum.filmorete.exeptions.MessageErrorValidUser.VALID_ERROR_USER_NOT_LOGIN;
+import static ru.yandex.practicum.filmorete.exeptions.message.ValidFilmErrorMessage.*;
+import static ru.yandex.practicum.filmorete.exeptions.message.UserErrorMessage.VALID_ERROR_USER_LOGIN_IS_WHITESPACE;
+import static ru.yandex.practicum.filmorete.exeptions.message.UserErrorMessage.VALID_ERROR_USER_NOT_LOGIN;
 
 @Slf4j
 public class ServiceValidators {
@@ -20,22 +19,22 @@ public class ServiceValidators {
         if (user.getName() == null || user.getName().isEmpty()) user.setName(user.getLogin());
 
         if (user.getLogin().isBlank()) {
-            throw new ExceptionValidationUser(VALID_ERROR_USER_NOT_LOGIN);
+            throw new ExceptionValidation(VALID_ERROR_USER_NOT_LOGIN);
         }
         if (user.getLogin().contains(" ")) {
-            throw new ExceptionValidationUser(VALID_ERROR_USER_LOGIN_IS_WHITESPACE);
+            throw new ExceptionValidation(VALID_ERROR_USER_LOGIN_IS_WHITESPACE);
         }
     }
 
     public static void checkValidFilm(@NotNull Film film) {
         if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
-            throw new ExceptionValidationFilm(VALID_ERROR_FILM_RELEASED_MIN);
+            throw new ExceptionValidation(VALID_ERROR_FILM_RELEASED_MIN);
         }
         if (film.getDuration() == null) {
-            throw new ExceptionValidationFilm(VALID_ERROR_FILM_NOT_DURATION);
+            throw new ExceptionValidation(VALID_ERROR_FILM_NOT_DURATION);
         }
         if (film.getDuration() < 0) {
-            throw new ExceptionValidationFilm(VALID_ERROR_FILM_DURATION_MIN);
+            throw new ExceptionValidation(VALID_ERROR_FILM_DURATION_MIN);
         }
     }
 }
