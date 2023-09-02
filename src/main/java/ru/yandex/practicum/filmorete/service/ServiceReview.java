d11e5efb06a29ee76bb0ad0a30cf95f86408d9e5
SinitsaBogdan <sinitsa.bogdan.1997@yandex.ru>
2023-09-03 02:12:38 +0300

    Update ServiceReview.java

--- src/main/java/ru/yandex/practicum/filmorete/service/ServiceReview.java
+++ src/main/java/ru/yandex/practicum/filmorete/service/ServiceReview.java
@@ -64,8 +64,10 @@ public class ServiceReview {
      * Добавление нового отзыва [ REVIEWS ].
      */
     public Review add(@NotNull Review reviews) {
-        if (userDao.find(reviews.getUserId()).isEmpty()) throw new ExceptionNotFoundUserStorage(VALID_ERROR_USER_ID_NOT_IN_COLLECTIONS);
-        if (filmDao.findFilm(reviews.getFilmId()).isEmpty()) throw new ExceptionNotFoundFilmStorage(VALID_ERROR_FILM_ID_NOT_IN_COLLECTIONS);
+        if (userDao.find(reviews.getUserId()).isEmpty())
+            throw new ExceptionNotFoundUserStorage(VALID_ERROR_USER_ID_NOT_IN_COLLECTIONS);
+        if (filmDao.findFilm(reviews.getFilmId()).isEmpty())
+            throw new ExceptionNotFoundFilmStorage(VALID_ERROR_FILM_ID_NOT_IN_COLLECTIONS);
         Long reviewId = reviewDao.insert(reviews.getContent(), reviews.getIsPositive(), reviews.getUserId(), reviews.getFilmId());
         eventsDao.insert(EventType.REVIEW, EventOperation.ADD, reviews.getUserId(), reviewId);
         return reviewDao.findByReviewId(reviewId).get();
